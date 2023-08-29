61ce63ac02ba10a02f013053a74fe839066bfaff
Altynbek <altinbekpin@gmail.com>
2023-08-29 09:40:01 +0600

    for docker build

--- api/main.py
+++ api/main.py
@@ -4,12 +4,12 @@ from tokenizers import Tokenizer
 from app import app
 import os
 from db import *
-from models import MyOwlReady
+# from models import MyOwlReady
 import school_termin_page, login_page, legacy_page, tagger, synomizer_page, ontology_page, termin_definition, enter_page, register, admin_page, intelect
 from db import db
 if __name__ == "__main__":
     load_dotenv()
-    app.s = MyOwlReady()
+    # app.s = MyOwlReady()
     app.tokenizer = Tokenizer.from_file("kazakh-bpe.tokenizer.json")
     app.run(port=5001, debug=True, host=os.environ.get("API_URL"))    
 
